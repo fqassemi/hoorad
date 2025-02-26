429154d84e41ed6336d5b9066f2d57755d147e83
Fred <farzad.qassemi@gmail.com>
2025-02-26 16:46:18 -0500

    Update image.jsx
    updating image.jsx to include class

--- components/templates/image.jsx
+++ components/templates/image.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import useGetImage from '@/hooks/api/image/useGetImg';
 
+const ImageComponent = ({ imageId, className }) => {
+  const { data: imageBlob, error } = useGetImage(imageId);
+
+  if (error) return <div>Failed to load image</div>;
+  if (!imageBlob) return <div>Loading...</div>;
+
+  const imageUrl = URL.createObjectURL(imageBlob);
+
+  return (
+    <img
+      src={imageUrl}
+      alt="Fetched Image"
+      className={className} // Apply the className here
+    />
+  );
+};
+
+export default ImageComponent;
+/*
+import React from 'react';
+import useGetImage from '@/hooks/api/image/useGetImg';
+
 const ImageComponent = ({ imageId }) => {
   const { data: imageBlob, error } = useGetImage(imageId);
 
@@ -13,3 +35,4 @@ const ImageComponent = ({ imageId }) => {
 };
 
 export default ImageComponent;
+*/
