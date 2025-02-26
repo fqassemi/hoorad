d97fbaf05fc91091619f8daf1c72a26f8603250f
Fred <farzad.qassemi@gmail.com>
2025-02-26 17:18:08 -0500

    Update image.jsx
    undo changes

--- components/templates/image.jsx
+++ components/templates/image.jsx
@@ -1,28 +1,6 @@
 import React from 'react';
 import useGetImage from '@/hooks/api/image/useGetImg';
 
-const ImageComponent = ({ imageId, className }) => {
-  const { data: imageBlob, error } = useGetImage(imageId);
-
-  if (error) return <div>Failed to load image</div>;
-  if (!imageBlob) return <div>Loading...</div>;
-
-  const imageUrl = URL.createObjectURL(imageBlob);
-
-  return (
-    <img
-      src={imageUrl}
-      alt="Fetched Image"
-      className={className} // Apply the className here
-    />
-  );
-};
-
-export default ImageComponent;
-/*
-import React from 'react';
-import useGetImage from '@/hooks/api/image/useGetImg';
-
 const ImageComponent = ({ imageId }) => {
   const { data: imageBlob, error } = useGetImage(imageId);
 
@@ -35,4 +13,3 @@ const ImageComponent = ({ imageId }) => {
 };
 
 export default ImageComponent;
-*/
