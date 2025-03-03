06479b3a52cb8212ffb193f95534486e7c3bb1a4
Fred <farzad.qassemi@gmail.com>
2025-03-03 18:53:38 -0500

    Update page.jsx
    aligning the images on course-card

--- app/(user)/course-detail/page.jsx
+++ app/(user)/course-detail/page.jsx
@@ -14,9 +14,11 @@ export default async function Home() {
          <div className="mb-20 mt-10 sm:mb-30 sm:mt-15">
             <p className="text-xl font-bold sm:text-3xl">همه ی دوره ها</p>
             <div className="mt-10 grid grid-cols-1 gap-7.5 sm:grid-cols-2 lg:grid-cols-3">
-               {coursesData?.map(item => (
-                  <CourseCard key={item?.id} detail={item} />
-               ))}
+              {coursesData?.map(item => (
+                <div key={item?.id} className="h-[400px]"> {/* Add fixed height container */}
+                  <CourseCard detail={item} />
+                </div>
+              ))}
             </div>
          </div>
       </section>
