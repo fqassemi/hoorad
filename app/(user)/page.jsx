d59700d0318f5e19e9c11ee93419ce238fdb28fb
Fred <farzad.qassemi@gmail.com>
2025-03-03 19:10:36 -0500

    Update page.jsx
    fixing the overlay problem in the main page

--- app/(user)/page.jsx
+++ app/(user)/page.jsx
@@ -1,3 +1,39 @@
+import CourseCard from '@/components/templates/course-card';
+import Blog from '@/components/templates/blogs';
+import fetchDataHandler from '@/lib/fetchDataHandler';
+
+export const revalidate = 60;
+
+export default async function Home() {
+  const coursesData = await fetchDataHandler('courses', {}, true);
+  const blogsData = await fetchDataHandler('blogs', {}, true);
+
+  return (
+    <section className="mx-auto max-w-1440 px-4 lg:px-[78px]">
+      {/* Courses Section */}
+      <div className="mb-20 mt-10 sm:mb-30 sm:mt-15">
+        <p className="text-xl font-bold sm:text-3xl">همه ی دوره ها</p>
+        <div className="mt-10 grid grid-cols-1 gap-7.5 sm:grid-cols-2 lg:grid-cols-3">
+          {coursesData?.map(item => (
+            <div key={item?.id} className="h-[400px]">
+              <CourseCard detail={item} />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Blogs Section */}
+      <div className="mb-20 mt-20">
+        <p className="text-xl font-bold sm:text-3xl">وبلاگ</p>
+        <div className="mt-10">
+          <Blog blogsDetail={blogsData} />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+{/*
 // Components
 import CourseCard from '@/components/templates/course-card';
 
@@ -30,3 +66,4 @@ export default async function Home() {
       </section>
    );
 }
+*/}
