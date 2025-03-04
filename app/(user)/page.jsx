f309cda77a5ee99706340aa90d1023841c0d8eca
Fred <farzad.qassemi@gmail.com>
2025-03-03 19:26:03 -0500

    Update page.jsx

--- app/(user)/page.jsx
+++ app/(user)/page.jsx
@@ -7,7 +7,7 @@ export const revalidate = 60;
 export default async function Home() {
   return (
     <section className="mx-auto max-w-1440 px-4 lg:px-[78px]">
-      {/* Courses Section */}
+
       <div className="mb-32 mt-10 sm:mb-40 sm:mt-15">
         <p className="text-xl font-bold sm:text-3xl">همه ی دوره ها</p>
         <div className="mt-10 grid grid-cols-1 gap-7.5 sm:grid-cols-2 lg:grid-cols-3">
@@ -19,7 +19,6 @@ export default async function Home() {
         </div>
       </div>
 
-      {/* Blogs Section */}
       <div className="mb-20 mt-32">
         <p className="text-xl font-bold sm:text-3xl">آخرین مقالات</p>
         <div className="mt-10">
